aefa07c0ac3f569ca042c3d011b11eb3f2bd95de
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-15 21:32:59 +0700

    Refactor

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import { Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   const theme = useTheme();
 
   return (
-    <Typography fontWeight="700" fontSize="1.7rem">
-      Bert<span style={{ color: theme.palette.primary.main }}>Flix</span>
+    <Typography variant="h6" fontWeight="bold" fontSize="1.7rem">
+      <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>
+        Bert
+      <span style={{ color: theme.palette.primary.main }}>Flix</span>
+      </Link>
     </Typography>
   );
 };
