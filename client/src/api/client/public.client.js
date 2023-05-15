a4099c169037ad4a17ee94fcbea7cc582089f4c4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-15 15:50:16 +0700

    Refactor

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "http://localhost:5000/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
