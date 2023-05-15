697173a50f8989503015ed035daa1ce374a564ae
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-15 20:57:03 +0700

    Refactor

--- client/src/redux/features/appStateSlice.js
+++ client/src/redux/features/appStateSlice.js
@@ -1,19 +1,19 @@
-// import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// export const appStateSlice = createSlice({
-//   name: "AppState",
-//   initialState: {
-//     appState: ""
-//   },
-//   reducers: {
-//     setAppState: (state, action) => {
-//       state.appState = action.payload;
-//     }
-//   }
-// });
+export const appStateSlice = createSlice({
+  name: "AppState",
+  initialState: {
+    appState: ""
+  },
+  reducers: {
+    setAppState: (state, action) => {
+      state.appState = action.payload;
+    }
+  }
+});
 
-// export const {
-//   setAppState
-// } = appStateSlice.actions;
+export const {
+  setAppState
+} = appStateSlice.actions;
 
-// export default appStateSlice.reducer;
+export default appStateSlice.reducer;
