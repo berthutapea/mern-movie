2f5a8feb8ea82ef717423828fba6675d5c371281
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-05-16 15:01:17 +0700

    Refactor

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://localhost:5000/api/v1/";
+const baseURL = "https://bertflix-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
